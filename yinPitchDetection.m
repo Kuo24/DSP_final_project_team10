dc70342818b2968f22291408af3c7726abce63c7
Kuo24 <107757162+Kuo24@users.noreply.github.com>
2024-01-16 00:38:48 +0800

    Add files via upload

--- yinPitchDetection.m
+++ yinPitchDetection.m
@@ -59,39 +59,3 @@ function pitch = yinPitchDetection(data, Fs)
         pitch = pitch_0;
     end
 end
-
-
-
-% step 4 - parabolic interpolation
-% period = zeros(1,nframes);
-% time = zeros(nframes,win);
-% f0 = zeros(nframes,win);
-% start = 1;
-% 
-% for i = 1:nframes
-%     if(lag(i) > 1 && lag(i) < win)
-%         alpha = d_norm(i,lag(i)-1);
-%         beta = d_norm(i,lag(i));
-%         gamma = d_norm(i,lag(i)+1);
-%         peak = 0.5*(alpha - gamma)/(alpha - 2*beta + gamma);
-%         ordinate needs to be calculated from d and not d_norm - see paper
-%         ordinate = d(i,lag(i)) - 0.25*(d(i,lag(i)-1) - d(i,lag(i)+1))*peak;
-%     else
-%         peak = 0;
-%     end
-%     1 needs to be subtracted from 1 due to matlab's indexing nature
-%     period(i) = (lag(i)-1) + peak;
-%     f0(i,:) = fs/period(i)*ones(1,win);
-%     time(i,:) = ((i-1)*win:i*win-1)/fs;
-%     
-% end
-% 
-% for silent frames estimated frequency should be 0Hz
-% if ~isempty(varargin)
-%     [f0] = silent_frame_classification(x_frame, f0);
-% end
-% 
-% f0 = reshape(f0',1,nframes*win);
-% time = reshape(time',1,nframes*win);
-% 
-% end
